056bb1302e8c4c5f5971d6a1aeb008b3a76c253f
Maria Anchugova <xeewisom@gmail.com>
2023-10-19 22:53:26 +0500

    Created create tour description fragment

--- app/src/main/java/com/hackaton/bonvoyage/coreFeatures/ui/createTour/description/DescriptionFragment.kt
+++ app/src/main/java/com/hackaton/bonvoyage/coreFeatures/ui/createTour/description/DescriptionFragment.kt
@@ -1,27 +1,28 @@
-package com.hackaton.bonvoyage.coreFeatures.ui.createHabitation.description
+package com.hackaton.bonvoyage.coreFeatures.ui.createTour.description
 
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.TextView
 import com.github.terrakok.cicerone.androidx.FragmentScreen
 import com.hackaton.bonvoyage.coreFeatures.BaseFragment
-import com.hackaton.bonvoyage.databinding.CreateHabitationDescriptionFragmentBinding
+import com.hackaton.bonvoyage.databinding.CreateTourDescriptionFragmentBinding
 
-
-class DescriptionFragment: BaseFragment<CreateHabitationDescriptionFragmentBinding>(CreateHabitationDescriptionFragmentBinding::inflate) {
+class DescriptionFragment: BaseFragment<CreateTourDescriptionFragmentBinding>(CreateTourDescriptionFragmentBinding::inflate) {
 
     override fun onStart() {
         super.onStart()
 
         binding.titleText.addTextChangedListener(CustomTextWatcher(binding))
         binding.descriptionEditText.addTextChangedListener(CustomTextWatcher(binding))
+        binding.durationEditText.addTextChangedListener(CustomTextWatcher(binding))
     }
 
-    class CustomTextWatcher(private val binding: CreateHabitationDescriptionFragmentBinding) : TextWatcher {
+    class CustomTextWatcher(private val binding: CreateTourDescriptionFragmentBinding) : TextWatcher {
         override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
         override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
             binding.btnNext.isClickable = !isEmpty(binding.titleText)
                     && !isEmpty(binding.descriptionEditText)
+                    && !isEmpty(binding.durationEditText)
         }
         override fun afterTextChanged(editable: Editable) {}
 
