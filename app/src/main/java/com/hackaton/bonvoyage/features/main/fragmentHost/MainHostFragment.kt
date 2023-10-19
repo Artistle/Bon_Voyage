e6564b092ecdb0ee396d3a7a99ea8a2c80ff9bae
nobryvkov <NObryvkov@tdera.ru>
2023-10-19 15:40:23 +0500

    __

--- app/src/main/java/com/hackaton/bonvoyage/features/main/fragmentHost/MainHostFragment.kt
+++ app/src/main/java/com/hackaton/bonvoyage/features/main/fragmentHost/MainHostFragment.kt
@@ -2,6 +2,9 @@ package com.hackaton.bonvoyage.features.main.fragmentHost
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.github.terrakok.cicerone.Command
 import com.github.terrakok.cicerone.NavigatorHolder
 import com.github.terrakok.cicerone.Router
 import com.github.terrakok.cicerone.androidx.AppNavigator
@@ -10,6 +13,7 @@ import com.hackaton.bonvoyage.R
 import com.hackaton.bonvoyage.databinding.ActivityAppBinding
 import com.hackaton.bonvoyage.databinding.ActivityMainBinding
 import com.hackaton.bonvoyage.features.main.main.MainFragment
+import com.hackaton.bonvoyage.features.main.profile.ProfileFragment
 import com.hackaton.core_di.nameDeep
 import com.hackaton.core_di.qualifiers.NavigationQualifiers
 import org.koin.android.ext.android.inject
@@ -20,16 +24,55 @@ class MainHostFragment: AppCompatActivity() {
     private val router: Router by inject(nameDeep(NavigationQualifiers.ROUTER_MAIN))
     private val holder: NavigatorHolder by inject(nameDeep(NavigationQualifiers.HOLDER_MAIN))
     private val navigator: AppNavigator by lazy {
-        AppNavigator(this, binding.fragmentContainerView.id)
+        customNavigator()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAppBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupBottomNavigationListener()
         holder.setNavigator(navigator)
         router.navigateTo(FragmentScreen{ MainFragment() })
-
     }
 
+
+    private fun setupBottomNavigationListener() {
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.main -> {
+                    router.navigateTo(FragmentScreen{ MainFragment() })
+                    true
+                }
+                R.id.account -> { true }
+                R.id.habitation -> { true }
+                R.id.messenger -> { true }
+                R.id.profile -> {
+                    router.navigateTo(ProfileFragment.getInstanceProfileFragment())
+                    true
+                }
+                else -> true
+            }
+        }
+    }
+
+    private fun customNavigator(): AppNavigator = object : AppNavigator(this, binding.fragmentContainerView.id) {
+
+        override fun setupFragmentTransaction(
+            screen: FragmentScreen,
+            fragmentTransaction: FragmentTransaction,
+            currentFragment: Fragment?,
+            nextFragment: Fragment
+        ) {
+
+            fragmentTransaction.setCustomAnimations(
+                R.anim.slide_enter_from_right, R.anim.slide_exit_to_left,
+                R.anim.slide_enter_from_left, R.anim.slide_exit_to_right
+            )
+        }
+
+        override fun applyCommands(commands: Array<out Command>) {
+            super.applyCommands(commands)
+        }
+    }
 }
